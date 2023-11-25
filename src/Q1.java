63e11319b14363b36529a3031ded15fed3c97f14
Azad <azadhaider62@gmail.com>
2023-11-25 20:18:24 +0300

    first commit in this repo

---
+++ src/Q1.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Q1 {
+    public static void main(String[] args) {
+        System.out.println("hello world"); // 1
+        for (int i = 1; i < 11; i++) { //2
+            System.out.println(i);
+
+           int array [] ={1,2,3,4};//3
+            for (int j = 0; j < array.length; j++) {
+                System.out.println(array[j]);
+            }
+//            Scanner inputarray = new Scanner();
+
+            }
+    }
+}
