73a5ba311cccaf3fc05ae3627be5d138aa77c3bf
Azad <azadhaider62@gmail.com>
2023-12-01 18:20:48 +0300

    create Students class

--- src/Q2.java
+++ src/Q2.java
@@ -1,4 +1,6 @@
 public class Q2 {
-                                 // 7 •	Array of objects (Students)
+                                 // 7 •	Array of objects (Students) first make a class Students
+
+
 
 }
